0a94251df9c346707c44b06ab2c015edb871d43b
Yuzhe Jin <827498681@qq.com>
2024-05-29 20:51:26 +0800

    update test example

--- PDGExtractor/Example.cs
+++ PDGExtractor/Example.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace PDGExtractor
+namespace PdgExtractor
 {
     public class Example
     {
@@ -29,6 +29,28 @@ namespace PDGExtractor
             int c = e.Sum(1, 2);
             Console.WriteLine(a + c);
             Function();
+        }
+
+        public static void IfElseWhile()
+        {
+            int a = 0;
+            float b = 10.0f;
+            a++;
+            int c = a;
+            if (b < 1)
+            {
+                a++;
+            }
+            else
+            {
+                a--;
+            }
+            a = 2;
+            c = (int)(b + a);
+            while (a < 10)
+            {
+                a++;
+            }
         }
     }
 }
